4ea75b01ac20402186d37da43cc6caa7180d6513
leandro <leandro.estrada@itsgroup.com.br>
2020-06-03 17:52:56 -0300

    Adding the addOrders view

---
+++ CoffeeHouse/Controllers/AddOrderViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AddOrderViewController.swift
+//  CoffeeHouse
+//
+//  Created by leandro de araujo estrada on 03/06/20.
+//  Copyright © 2020 leandro de araujo estrada. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddOrderViewController: UIViewController{
+    
+    
+    
+}
---
+++ CoffeeHouse/Controllers/OrdersTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  OrdersTableViewController.swift
+//  CoffeeHouse
+//
+//  Created by leandro de araujo estrada on 03/06/20.
+//  Copyright © 2020 leandro de araujo estrada. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class OrdersTableViewController: UITableViewController{
+    
+    
+    
+}
